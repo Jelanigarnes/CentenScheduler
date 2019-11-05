7caee8d34807e9708c677fa0a95d153875d84fa3
Jelani Garnes <35810049+Jelanigarnes@users.noreply.github.com>
2019-11-05 16:48:17 -0500

    Add project files.

---
+++ CentenScheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CentenScheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
